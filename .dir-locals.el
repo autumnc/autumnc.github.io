493fe9c9130645ebda4f9ce78b9f7df02eeb5bc7
autumnc <yangwenzu@163.com>
2020-06-06 01:35:10 +0800

    redo

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+(("content-org/Notes/"
+  . ((org-mode . ((eval . (org-hugo-auto-export-mode)))))))
+(("content-org/digest/"
+  . ((org-mode . ((eval . (org-hugo-auto-export-mode)))))))
+(("content-org/Tech/"
+  . ((org-mode . ((eval . (org-hugo-auto-export-mode)))))))
