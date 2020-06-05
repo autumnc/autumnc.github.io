d27836001c56a023995675f532af1e18c75bb9e7
autumnc <yangwenzu@163.com>
2020-06-06 02:20:39 +0800

    clear

--- .dir-locals.el
+++
@@ -1,6 +0,0 @@
-(("content-org/Notes/"
-  . ((org-mode . ((eval . (org-hugo-auto-export-mode)))))))
-(("content-org/digest/"
-  . ((org-mode . ((eval . (org-hugo-auto-export-mode)))))))
-(("content-org/Tech/"
-  . ((org-mode . ((eval . (org-hugo-auto-export-mode)))))))
